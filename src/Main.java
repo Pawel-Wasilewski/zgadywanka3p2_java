b84b9572f69e904bc5f235116562afab14554203
Pawel-Wasilewski <wasilewski.pawel2007@gmail.com>
2024-09-06 10:14:33 +0200

    losowanie liczby calkowitej

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        double wylosowana_liczba = Math.random() * 100 + 1;
+        int wylosowana_liczba = (int) (Math.random() * 100 + 1);
+        /*
+            rzutowanie danych
+         */
         System.out.println(wylosowana_liczba);
     }
 }
