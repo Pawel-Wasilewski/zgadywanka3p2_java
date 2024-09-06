d361ffc8e4abfe18e41ff606fa152c1e3c2215ad
Pawel-Wasilewski <wasilewski.pawel2007@gmail.com>
2024-09-06 11:19:28 +0200

    dalsze okomentowanie kodu

--- src/Main.java
+++ src/Main.java
@@ -34,50 +34,53 @@ public class Main {
             If Else itd.
         */
 
-        int pochybienie = 0;
+        /*
+            int pochybienie = 0;
 
-        if (liczba_docelowa == wylosowana_liczba) {
-            System.out.println("Zgadłeś :D");
-        } else {
-            System.out.println("Nie zgadłeś :(");
+            if (liczba_docelowa == wylosowana_liczba) {
+                System.out.println("Zgadłeś :D");
+            } else {
+                System.out.println("Nie zgadłeś :(");
 
-            /*
-                Zmiana danych - wyrażenia warunkowe
-            */
 
-            pochybienie = liczba_docelowa > wylosowana_liczba ? liczba_docelowa - wylosowana_liczba : wylosowana_liczba - liczba_docelowa;
+                    Zmiana danych - wyrażenia warunkowe
 
-            System.out.println("Pochybienie o: " + pochybienie);
 
-            pochybienie = pochybienie / 10;
+                pochybienie = liczba_docelowa > wylosowana_liczba ? liczba_docelowa - wylosowana_liczba : wylosowana_liczba - liczba_docelowa;
 
-            switch (pochybienie) {
-                case 0:
-                    System.out.println("było blisko byq");
-                    break;
-                case 1:
-                    System.out.println("wystarczająco");
-                    break;
-                case 2:
-                    System.out.println("Mogło być lepiej");
-                    break;
-                default:
-                    System.out.println("FF 15");
+                System.out.println("Pochybienie o: " + pochybienie);
+
+                pochybienie = pochybienie / 10;
+
+                switch (pochybienie) {
+                    case 0:
+                        System.out.println("było blisko byq");
+                        break;
+                    case 1:
+                        System.out.println("wystarczająco");
+                        break;
+                    case 2:
+                        System.out.println("Mogło być lepiej");
+                        break;
+                    default:
+                        System.out.println("FF 15");
+                }
+
+
+                    Inny zapis switcha
+
+
+                System.out.println(
+                        switch (pochybienie) {
+                            case 0 -> "Było blisko byq";
+                            case 1 -> "Wystarczająco";
+                            case 2 -> "Mogło być lepiej";
+                            default -> "FF 15";
+                        }
+                );
             }
+        */
 
-            /*
-                Inny zapis switcha
-            */
-
-            System.out.println(
-                    switch (pochybienie) {
-                        case 0 -> "Było blisko byq";
-                        case 1 -> "Wystarczająco";
-                        case 2 -> "Mogło być lepiej";
-                        default -> "FF 15";
-                    }
-            );
-        }
         /*
             Pętla For
         */
